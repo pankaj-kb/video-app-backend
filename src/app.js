bea2dd75faaf9d3a6dc32a40a387269ee101ed6a
Pankaj Bhadane <pankajb2601@gmail.com>
2023-12-15 16:10:56 +0530

    added utils to handle error and async/promise functions

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-
+// setting cors
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
+// Config
 app.use(express.json({ limit: "20kb" }))
 app.use(express.urlencoded({ extended: true, limit: "20kb" }))
 app.use(express.static("public"))
