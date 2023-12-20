55d8400976c1204cbe22cfd8fb5982cdfea8d66d
Pankaj Bhadane <pankajb2601@gmail.com>
2023-12-20 22:56:19 +0530

    fixed username typo

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema(
     {
-        userName: {
+        username: {
             type: String,
             required: true,
             unique: true,
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken = function () {
     return jwt.sign({
         _id: this._id,
         email: this.email,
-        userName: this.userName,
+        username: this.username,
         fullName: this.fullName
     }, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: process.env.ACCESS_TOKEN_EXPIRY
