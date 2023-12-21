77ba2b42bbcbd523984ffe2c9c0ad71eda77360a
Pankaj Bhadane <pankajb2601@gmail.com>
2023-12-22 00:10:56 +0530

    created auth middleware and logout functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -139,7 +139,35 @@ const loginUser = asyncHandler(async (req, res) => {
         )
 })
 
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id, {
+        $set: {
+            refreshToken: undefined
+        }
+    },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(
+            new APIResponse(200, {}, "User is logged out successfully")
+        )
+})
+
 export {
     registerUser,
     loginUser,
+    logoutUser
 }
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user.model.js";
+import { APIError } from "../utils/APIError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { jwt } from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+        if (!token) {
+            throw new APIError(401, "Unauthorized request")
+        }
+
+        const decotedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findById(decotedToken?._id).select("-password -refreshToken")
+
+        if (!user) {
+            // TODO: related to frontend.
+            throw new APIError(401, "Invalid Access Token")
+        }
+
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new APIError(401, error?.message || "Invalid Access token")
+    }
+})
