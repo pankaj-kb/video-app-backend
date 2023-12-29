4f721f09be1b4c7bbb3f014225d0f03a390a8ced
Pankaj Bhadane <pankajb2601@gmail.com>
2023-12-29 21:10:12 +0530

    added another approch to store likes

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -13,13 +13,21 @@ const likeSchema = new mongoose.Schema({
         type: Schema.Types.ObjectId,
         ref: "Tweet"
     },
+    // on thinking that  it will be an array storing the refference of users who liked.
+    // likedBy: {
+    //     type: [
+    //         {
+    //             type: Schema.Types.ObjectId,
+    //             ref: "User"
+    //         }
+    //     ]
+    // }
+
+    // another approach which concerns that each like will be different document.
+
     likedBy: {
-        type: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "User"
-            }
-        ]
+        type: Schema.Types.ObjectId,
+        ref: "User"
     }
 }, {
     timestamps: true
