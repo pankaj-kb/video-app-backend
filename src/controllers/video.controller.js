a13b0a77cdf2988eb0fb7b7daac56929c85bf805
Pankaj Bhadane <pankajb2601@gmail.com>
2024-01-09 16:17:01 +0530

    created two different methods to show all the videos

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,16 +1,20 @@
 import mongoose, { isValidObjectId } from "mongoose"
 import { Video } from "../models/video.model.js"
 import { User } from "../models/user.model.js"
-import { ApiError } from "../utils/ApiError.js"
+import { APIError, ApiError } from "../utils/ApiError.js"
 import { APIResponse, ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 //TODO: get all videos based on query, sort, pagination
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    const { page = 1, limit = 10, query, sortBy, sortType } = req.query
 
     const searchQuery = query;
+
+    if (!(sortType === "desc" || sortType === "asc")) {
+        throw new APIError(401, "Incorrect query");
+    }
 
     const allVideos = await Video.aggregate([
         {
@@ -37,6 +41,53 @@ const getAllVideos = asyncHandler(async (req, res) => {
     return res
         .status(200)
         .json(new APIResponse(200, allVideos, "all Videos fetched"));
+})
+
+const getAllVideosByUser = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+
+    const searchQuery = query;
+
+    if (userId.length === 0) {
+        throw new APIError(401, "kindly add userId")
+    }
+
+    const checkUserExist = await User.findById(userId)
+
+    if (!checkUserExist) {
+        throw new APIError(400, "User does not exist")
+    }
+
+    if (!(sortType === "desc" || sortType === "asc")) {
+        throw new APIError(401, "Incorrect query");
+    }
+
+    const allVideosbyUser = await Video.aggregate([
+        {
+            $match: {
+                owner: mongoose.Types.ObjectId(userId),
+                title: {
+                    $regex: new RegExp(searchQuery, 'i')
+                }
+            }
+        },
+        {
+            $sort: {
+                [sortBy]: sortType === 'desc' ? -1 : 1
+            }
+        },
+        {
+            $skip: (page - 1) * limit
+        },
+        {
+            $limit: parseInt(limit)
+        }
+
+    ])
+
+    return res
+        .status(200)
+        .json(new APIResponse(200, allVideosbyUser, "all Videos fetched"));
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -66,6 +117,7 @@ const togglePublishStatus = asyncHandler(async (req, res) => {
 
 export {
     getAllVideos,
+    getAllVideosByUser,
     publishAVideo,
     getVideoById,
     updateVideo,
