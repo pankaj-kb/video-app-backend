32c4cab125175c090ff44c8874e462435c90c5d9
pankaj-kb <pankajb2601@gmail.com>
2024-01-14 00:29:03 +0530

    completed add comment controller function

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,11 +4,12 @@ import { Video } from "../models/video.model.js"
 import { APIError } from "../utils/APIError.js"
 import { APIResponse } from "../utils/APIResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { User } from "../models/user.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const { videoId } = req.params
-    const { page, limit, sortBy, sortType} = req.query
+    const { page, limit, sortBy, sortType } = req.query
     const video = await Video.findOne({ _id: videoId })
 
     if (!video) {
@@ -54,19 +55,53 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
     if (paginatedComments.totalComments === 0) {
         return res
-        .status(200)
-        .json(new APIResponse(200, null, "No Comments Found."))
+            .status(200)
+            .json(new APIResponse(200, null, "No Comments Found."))
     }
 
     return res
-    .status(200)
-    .json(new APIResponse(200, paginatedComments, "All Comments fetched successfully."))
+        .status(200)
+        .json(new APIResponse(200, paginatedComments, "All Comments fetched successfully."))
 
 
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+
+    const { videoId } = req.params
+
+    const video = await Video.findOne({ _id: videoId })
+
+    if (!video) {
+        throw new APIError(404, "Video not found/exist.")
+    }
+
+    const { comment } = req.body
+
+    if (!comment) {
+        throw new APIError(401, "Comment is required.")
+    }
+
+    const owner = await User.findById(req.user?._id)
+
+    if (!owner) {
+        throw new APIError(401, "Kindly login first to add comment.")
+    }
+
+    const addComment = await Comment.create({
+        comment: comment,
+        video: videoId,
+        owner: owner._id
+    })
+
+    if (!addComment) {
+        throw new APIError(401, "Something went wrong while adding comment.")
+    }
+
+    return res
+        .status(200)
+        .json(new APIResponse(201, addComment, "Comment added successfully."))
 })
 
 const updateComment = asyncHandler(async (req, res) => {
